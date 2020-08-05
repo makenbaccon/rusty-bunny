018d0ccd93ab3648a9f9bb6e16f2708de5a13cf8
Andrew Fischer <makenbaccon@gmail.com>
2020-08-05 10:25:59 -0700

    Add "l8", "yt", "rdt", and "msg" commands

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,19 @@ fn search(cmd: String) -> Redirect {
 
     // Keep in alphabetic order
     let redirect_url = match command {
+        // google tools
         "cal" => String::from("https://calendar.google.com/"),
         "drive" => String::from("https://drive.google.com/"),
+        "mail" => String::from("https://mail.google.com/"),
+        "msg" => String::from("https://messages.google.com/web/conversations"),
+        // coding tools
         "gh" => utils::github::construct_github_url(&cmd),
         "ih" => String::from("https://indiehackers.com"),
-        //"l8" => String::from("http://localhost:8000/"),
-        "mail" => String::from("https://mail.google.com/"),
+        "l8" => String::from("http://localhost:8000/"),
+        // fun tools
+        "yt" => String::from("https://www.youtube.com/feed/subscriptions"),
+        "rdt" => String::from("https://www.reddit.com/"),
+        // misc tools
         "tw" => utils::twitter::construct_twitter_url(&cmd),
         // If no match, we search on Google
         _ => utils::google::construct_google_search_url(&cmd),
